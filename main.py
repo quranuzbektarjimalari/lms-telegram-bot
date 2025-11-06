3a8b71eb1909337d5fa930ffe37a9d1b53cbd960
mirzabek <yaqubovmirzobek@gmail.com>
2025-11-06 19:23:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -437,7 +437,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     creds = get_credentials_for(chat_id)
     if creds:
         await update.message.reply_text(
-            "👋 Assalomu alaykum! Siz oldin tizimga kirgansiz. Iltimos, menyudagi tugmalardan foydalaning:",
+            "👋 Assalomu alaykum! Siz oldin tizimga kirgansiz. \nIltimos, menyudagi tugmalardan foydalaning:",
             reply_markup=keyboard,
         )
     else:
@@ -631,7 +631,7 @@ async def send_results(update: Update, fullname, tests, assignments):
                       short_deadline = datetime.strptime(deadline, "%d-%m-%Y %H:%M:%S").strftime("%d-%m-%Y %H:%M")
                     except Exception:
                       short_deadline = deadline
-                    msg += f"📘 Test: _{title} ([ko‘rish]({link}))_\n🕒 Tugash: {short_deadline} {left} \n📖 Fan: {subject}\n\n"
+                    msg += f"📘 Test: _{title}_ _([ko‘rish]({link}))_\n⏱️ Tugash: {short_deadline} _{left}_ \n📓 Fan: {subject}\n\n"
 
 
             if assignments:
@@ -642,7 +642,7 @@ async def send_results(update: Update, fullname, tests, assignments):
                       short_deadline = datetime.strptime(deadline, "%d-%m-%Y %H:%M:%S").strftime("%d-%m-%Y %H:%M")
                     except Exception:
                       short_deadline = deadline                  
-                    msg += f"📕 Topshiriq: _{title}_ ([ko‘rish]({link}))_\n🕒 Tugash: {short_deadline} {left} \n📖 Fan: {subject}\n\n"
+                    msg += f"📕 Topshiriq: _{title}_ _([ko‘rish]({link}))_\n⏱️ Tugash: {short_deadline} _{left}_ \n📓 Fan: {subject}\n\n"
             
 
 # === 6. Botni ishga tushirish ===
